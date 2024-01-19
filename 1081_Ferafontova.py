a5c21b3e8a9bcfb76455594b21e5f9641bd82837
Irina <vega_is@mail.ru>
2024-01-19 15:14:22 +0400

    first commit

---
+++ 1081_Ferafontova.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# region  кусок с вычислениями
+
+x = np.arange(-3, 3, 0.1) # диапазон x
+y = np.arange(-3, 3, 0.1) # диапазон y
+
+X, Y = np.meshgrid(x, y)
+Z = X**2 - Y**2 + Y
+
+r = np.random.normal(0, 2, size=Z.shape) # белый шум
+F = Z+r
+
+A = np.ones((len(x) * len(y), 3))
+A[:, 1] = X.flatten()
+A[:, 2] = Y.flatten()
+result = np.linalg.lstsq(A, F.flatten())
+
+ZA = np.dot (A, result[0]).reshape(F.shape) # аппроксимирующая поверхность
+
+# endregion
+
+# region все графики на одной картинке
+
+fig = plt.figure ()
+ax = fig.add_subplot (111, projection='3d')
+ax.plot_surface (X, Y, Z, cmap = 'inferno', alpha = 0.5, label ='Исходные данные') # исходный график
+ax.plot_surface (X, Y, F, cmap = 'viridis', alpha = 0.5, label = 'Исходные данные + ШУМ') # график с шумом
+ax.plot_surface (X, Y, ZA, color = 'blue', alpha = 0.5, label = 'Аппроксимация') # график апроксимации
+ax.set_xlabel ('x')
+ax.set_ylabel ('y')
+ax.set_zlabel ('z')
+ax.legend ()
+plt.savefig ('Все_данные.png')
+plt.show ()
+#endregion
+
+# region график шум + апроксимация 
+
+fig = plt.figure ()
+ax = fig.add_subplot (111, projection = '3d')
+ax.plot_surface (X, Y, F, cmap = 'viridis', alpha = 0.5, label = 'Исходные данные + ШУМ') # график с шумом
+plt.savefig ('Исходные_данные+ШУМ.png')
+ax.plot_surface (X, Y, ZA, color = 'blue', alpha = 0.5, label = 'Аппроксимация')# график апроксимации
+plt.savefig ('Аппроксимация.png')
+ax.set_xlabel ('x')
+ax.set_ylabel ('y')
+ax.set_zlabel ('z')
+ax.legend ()
+plt.show ()
+#endregion
+
+# region графики отдельно    
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot (131, projection = '3d') # исходный график
+ax1.plot_surface (X, Y, Z, cmap = 'inferno', alpha = 0.5, label = 'Исходные данные')
+ax1.set_xlabel ('x')
+ax1.set_ylabel ('y')
+ax1.set_zlabel ('z')
+ax1.legend ()
+
+ax2 = fig.add_subplot (132, projection='3d') # график с шумом
+ax2.plot_surface (X, Y, F, cmap = 'viridis', alpha = 0.5, label = 'Исходные данные + ШУМ')
+ax2.set_xlabel ('x')
+ax2.set_ylabel ('y')
+ax2.set_zlabel ('z')
+ax2.legend ()
+
+ax3 = fig.add_subplot (133, projection = '3d') # график апроксимации
+ax3.plot_surface (X, Y, ZA, color ='blue', alpha = 0.5, label = 'Аппроксимация')
+ax3.set_xlabel ('x')
+ax3.set_ylabel ('y')
+ax3.set_zlabel ('z')
+ax3.legend ()
+
+plt.savefig ('ОТДЕЛЬНЫЕ ГРАФИКИ.png')# отображение графиков
+plt.show ()
+#endregion
+#region
+print ('                           ', '|свободный член|', '|коэфф при х|', '|коэфф при у|')
+print ('Коэффициенты аппроксимации:', result[0]) #коэфф апроксимации
+print ('Ошибка апроксимации:', result[1] / (len(x) * len(y))) # ошибка аппроксимации
+print ('Кортеж значений:', result)
+#endregion
